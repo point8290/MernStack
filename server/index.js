c3bad1fdecebb561be4cf98eef7a87348bb2dd50
Krishna Meghwal <krishnameghwal635@gmail.com>
2024-01-04 22:19:54 +0530

    Changes related to database connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from "express"
-
-const app = express()
-const PORT = process.env.PORT || 3000
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 3000;
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(DB_CONNECTION_STRING)
+mongoose.connection.once("connection",()=>{
+    console.log('Connected to MongoDB')
+});
+mongoose.connection.on("error",()=>{
+    console.log('Error while connecting to MongoDB')
+});
 app.listen(PORT, ()=>{
     console.log(`Server is running on port: ${PORT}`)
 })
