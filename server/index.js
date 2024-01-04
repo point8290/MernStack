4a0bd2ea9cadcdbecea659dbae13d8c60dfc1b61
Krishna Meghwal <krishnameghwal635@gmail.com>
2024-01-04 22:35:49 +0530

    Created folder structure for routes, models and controllers in backend

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
-mongoose.connect(DB_CONNECTION_STRING)
-mongoose.connection.once("connection",()=>{
-    console.log('Connected to MongoDB')
+mongoose.connect(DB_CONNECTION_STRING);
+mongoose.connection.once("connection", () => {
+  console.log("Connected to MongoDB");
 });
-mongoose.connection.on("error",()=>{
-    console.log('Error while connecting to MongoDB')
+mongoose.connection.on("error", () => {
+  console.log("Error while connecting to MongoDB");
 });
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port: ${PORT}`)
-})
+app.use("/api/user/", userRoutes);
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
