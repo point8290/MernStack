39b52792d8ea2caf29de510097a39c85b5dfe6a9
Krishna Meghwal <krishnameghwal635@gmail.com>
2024-01-04 21:44:49 +0530

    Created Routes Links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,22 @@
 import { BrowserRouter, Routes,Route} from "react-router-dom"
-import Home from "../pages/Home"
-import Signin from "../pages/Signin"
-import Signup from "../pages/Signup"
-import About from "../pages/About"
-import Profile from "../pages/Profile"
-
+import Home from "./pages/Home"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/sign-in" element={<Signin />}/>
-      <Route path="/sign-up" element={<Signup />}/>
-      <Route path="/about" element={<About />}/>
-      <Route path="/profile" element={<Profile />}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/sign-in" element={<Signin />}/>
+        <Route path="/sign-up" element={<Signup />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/profile" element={<Profile />}/>
+      </Routes>
     </BrowserRouter>
   )
 }
