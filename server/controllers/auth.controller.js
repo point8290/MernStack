75e3b2207a469a45d28266910a0d4a3ea758b857
Krishna Meghwal <krishnameghwal635@gmail.com>
2024-01-05 21:16:34 +0530

    Added Error Handler Middleware

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     const hashedPassword = await bcryptjs.hash(password, 10);
@@ -11,6 +11,6 @@ export const signUp = async (req, res) => {
     });
     res.status(201).json({ message: "User created succesfully" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
--- server/index.js
+++ server/index.js
@@ -21,6 +21,16 @@ mongoose.connection.on("error", () => {
 app.use("/api/user/", userRouter);
 app.use("/api/auth/", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
