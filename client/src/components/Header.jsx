73f75553b763d5dd3637caeef8e6ab1fed91c45e
Krishna Meghwal <krishnameghwal635@gmail.com>
2024-01-05 22:10:58 +0530

    Implemented signup functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 function Header() {
+  console.log("Header re-rendered");
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-2">
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,11 +1,53 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 function Signup() {
+  console.log("Signup re-rendered");
+
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (event) => {
+    setFormData((data) => ({
+      ...data,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      setError(null);
+      const resp = await fetch("/api/auth/sign-up", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await resp.json();
+      console.log("data", data);
+      setLoading(false);
+      if (data.success == false) {
+        setError(data.message);
+      } else {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+      console.log(error);
+    }
+  };
   return (
     <section className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-bold my-7">Sign up</h1>
-      <form className="flex flex-col gap-4 ">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           id="username"
+          onChange={handleChange}
           name="username"
           className="border p-3 rounded-lg"
           type="text"
@@ -13,6 +55,7 @@ function Signup() {
         />
         <input
           id="email"
+          onChange={handleChange}
           name="email"
           className="border p-3 rounded-lg"
           type="email"
@@ -20,13 +63,19 @@ function Signup() {
         />
         <input
           id="password"
+          onChange={handleChange}
           name="password"
           className="border p-3 rounded-lg"
           type="password"
           placeholder="Password"
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign up
+        <button
+          disabled={loading}
+          className={`bg-slate-700 text-white p-3 rounded-lg ${
+            loading ? "" : "uppercase"
+          } hover:opacity-95 disabled:opacity-80`}
+        >
+          {loading ? "Signing up..." : "Sign up"}
         </button>
       </form>
 
@@ -36,6 +85,7 @@ function Signup() {
           <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
+      {error && <p className="text-red-600 py-2">{error}</p>}
     </section>
   );
 }
