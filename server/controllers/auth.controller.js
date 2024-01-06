353f2dbceb84fa045148344818fb505841ce8a74
Krishna Meghwal <krishnameghwal635@gmail.com>
2024-01-06 10:50:42 +0530

    Implemented sign in REST

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signUp = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
@@ -10,6 +12,30 @@ export const signUp = async (req, res, next) => {
       password: hashedPassword,
     });
     res.status(201).json({ message: "User created succesfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const dbUser = await User.findOne({ email });
+    if (dbUser) {
+      const user = await bcryptjs.compare(password, dbUser.password);
+      if (user) {
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+        const { password: pass, ...rest } = dbUser._doc;
+        res
+          .cookie("access-token", token, { httpOnly: true })
+          .status(200)
+          .json({ user: rest });
+      } else {
+        return next(errorHandler(401, "Wrong credentials"));
+      }
+    } else {
+      return next(errorHandler(404, "User not found"));
+    }
   } catch (error) {
     next(error);
   }
