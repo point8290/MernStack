7f77678f9d47939000012c6bc4171274108c7f79
Krishna Meghwal <krishnameghwal635@gmail.com>
2024-01-17 22:49:47 +0530

    added serach route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -60,3 +60,46 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const offer = req.query.offer;
+    const furnished = req.query.furnished;
+    const parking = req.query.parking;
+    const type = req.query.type;
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    if (offer == undefined || offer == false) {
+      offer = { $in: [true, false] };
+    }
+    if (furnished == undefined || furnished == false) {
+      furnished = { $in: [true, false] };
+    }
+    if (parking == undefined || parking == false) {
+      parking = { $in: [true, false] };
+    }
+    if (type == undefined || type == "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+    if (listings) {
+      return res.status(200).json(listings);
+    } else {
+      return next(errorHandler(404, "Listings not found"));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -3,6 +3,7 @@ import {
   create,
   deleteListing,
   getListing,
+  getListings,
   edit,
 } from "../controllers/listing.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
@@ -12,5 +13,6 @@ router.post("/create", verifyUser, create);
 router.get("/:id", getListing);
 router.post("/edit/:id", verifyUser, edit);
 router.delete("/delete/:id", verifyUser, deleteListing);
+router.get("/", getListings);
 
 export default router;
