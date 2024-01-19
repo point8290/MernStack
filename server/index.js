5cf2bfff76905eb2d63a00dd66e581475c7c0f8c
Krishna Meghwal <krishnameghwal635@gmail.com>
2024-01-19 16:47:08 +0530

    deployement changes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -21,10 +22,17 @@ mongoose.connection.on("error", () => {
   console.log("Error while connecting to MongoDB");
 });
 
+const __dirname = path.resolve();
+
 app.use("/api/user/", userRouter);
 app.use("/api/auth/", authRouter);
 app.use("/api/listing/", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist/")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
